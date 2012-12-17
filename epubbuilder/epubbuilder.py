cb565116e87ce8ddc003578b81eca8040dcaffed
Mathieu Comandon <strycore@gmail.com>
2012-12-17 08:11:04 +0100

    verify existence of mimetype instead

--- epubbuilder/epubbuilder.py
+++ epubbuilder/epubbuilder.py
@@ -394,10 +394,7 @@ class EpubBook:
         """
         for item in self.get_all_items():
             content = item.content
-            if not hasattr(item, 'mime_type'):
-                print "Item %s has no mime type" % item
-                continue
-            if not item.mime_type.startswith('image'):
+            if item.mime_type and not item.mime_type.startswith('image'):
                 content = content.encode('utf-8')
             zip_outout.writestr(
                 os.path.join('OEBPS', item.dest_path),
